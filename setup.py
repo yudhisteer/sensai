dd965d9d62003be3fa2adc5b62128fa8ad3615b9
yudhisteer <yudhisteer.chin@gmail.com>
2025-03-28 01:13:43 -0700

    refactor: desc in setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="sensai",
     version="0.1.0",
-    description="AI agents for sensors",
+    description="AI Agents for Sensors",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="",
-    author_email="",
+    author="yudhisteer",
+    author_email="yudhisteer@gmail.com",
     packages=find_packages(),
     python_requires=">=3.12",
     install_requires=[
