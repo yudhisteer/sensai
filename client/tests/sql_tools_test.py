f37ccd8e163fdb8d1856d102346a858bd62284ad
yudhisteer <yudhisteer.chin@gmail.com>
2025-04-06 01:09:11 -0700

    chore: file renamings

--- client/tests/sql_tools_test.py
+++
@@ -1,6 +0,0 @@
-from client.agents.sql_agent.tools import execute_sql_query
-
-if __name__ == "__main__":
-    sql_query = "SELECT * FROM temperature_readings"
-    response = execute_sql_query(sql_query, "http://127.0.0.1:8000/temperature/sql")
-    print(response)
